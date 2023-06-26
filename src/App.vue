d51bcd19fb341c0abeff9f0842761e4407c41ab2
CosimoTramonte <cosimotramonte7@gmail.com>
2023-06-26 15:10:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+
+<script>
+import { store } from './store/store.js';
+import axios from 'axios';
+
+export default {
+
+  data(){
+    return{
+      projects:[],
+    }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl + 'projects')
+          .then(result=>{
+            this.projects = result.data;
+            console.log(this.projects);
+          })
+    }
+  },
+
+  mounted(){
+    this.getApi();
+  }
+}
+</script>
+
+<template>
+    
+    <div class="container">
+      <h1>Projects List</h1>
+    </div>
+
+</template>
+
+<style>
+
+</style>
