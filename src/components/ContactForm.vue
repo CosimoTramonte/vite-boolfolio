9a9718a13c34b980d3fd3beb493ff120ff37942d
CosimoTramonte <cosimotramonte7@gmail.com>
2023-06-29 15:06:21 +0200

    add form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,45 @@
+
+<script>
+export default {
+    name:'ContactForm',
+    data(){
+        return{
+            name: '',
+            email: '',
+            message: '',
+        }
+    },
+
+    methods:{
+        sendForm(){
+            const $data = {
+                name: this.name,
+                email: this.email,
+                message: this.message,
+            }
+            console.log($data);
+        }
+    }
+}
+</script>
+
+<template>
+
+    <form @submit.prevent="sendForm()">
+        <div class="w-100 py-3">
+            <input v-model.trim="name" type="text" class="form-control" placeholder="Name">
+        </div>
+        <div class="w-100 py-3">
+            <input v-model.trim="email" type="email" class="form-control" placeholder="Email">
+        </div>
+        <div class="w-100 py-3">
+            <textarea v-model.trim="message" class=" form-control w-100" cols="30" rows="10" placeholder="Message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Invia</button>
+    </form>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,12 +1,19 @@
 <script>
+import ContactForm from '../components/ContactForm.vue';
 export default {
     name:"Contacts",
+
+    components:{
+        ContactForm
+    }
 }
 </script>
 
 <template>
     <div class="page-wrapper">
         <h1>Contacts</h1>
+
+        <ContactForm/>
     </div>
 </template>
 
